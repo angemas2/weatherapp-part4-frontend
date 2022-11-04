06ac675386b633f258d62cc2cce9b5b92fffc5cf
Angélique <angelique.massolo@gmail.com>
2022-11-04 17:09:06 +0100

    deploy front

---
+++ login.js
@@ -0,0 +1,36 @@
+// Insert your code here
+
+document.querySelector("#register").addEventListener("click", function () {
+  let email = document.querySelector("#registerEmail").value;
+  let password = document.querySelector("#registerPassword").value;
+
+  fetch("http:localhost:3000/users/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.result) {
+       window.location.assign("index.html");
+      }
+    });
+});
+
+document.querySelector("#connection").addEventListener("click", function () {
+  let email = document.querySelector("#connectionEmail").value;
+  let password = document.querySelector("#connectionPassword").value;
+
+  fetch("http:localhost:3000/users/signin", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+      window.location.assign("index.html");
+      }
+    });
+});
